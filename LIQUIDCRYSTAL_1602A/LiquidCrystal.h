2e8e717f302967a2fceff804bd66b2ce0bdf8b93
christian <73924544+WhatsAssembly@users.noreply.github.com>
2022-12-17 01:55:38 -0800

    Update LiquidCrystal.h

--- LIQUIDCRYSTAL_1602A/LiquidCrystal.h
+++ LIQUIDCRYSTAL_1602A/LiquidCrystal.h
@@ -6,13 +6,13 @@
 //			Commands
 //
 /****************************/
-#define CLEAR_DISPALY 			0x01 //			Clear Display
+#define CLEAR_DISPALY 			    0x01 //			Clear Display
 #define RETURN_HOME 				0x02 //			RETURN HOME
 #define ENTRYMODESET 				0x04 //			ENTRY MODE SET
 #define CURSOR_SHIFT 				0x10 //			CURSOR || DISPLAY SHIFT
 #define FUNCTION_SET 				0x20 //			FUNCTION SET
-#define SET_CGRAM_ADDRESS 	0x40 //			SET CGRAM ADDRESS
-#define SET_DDRAM_ADDRESS 	0x80 //			SET DDRAM ADDRESS
+#define SET_CGRAM_ADDRESS 	        0x40 //			SET CGRAM ADDRESS
+#define SET_DDRAM_ADDRESS 	        0x80 //			SET DDRAM ADDRESS
 
 
 /***************************/
@@ -24,10 +24,10 @@
 #define DISPLAY_OFF 				0x00
 #define CURSOR_ON 					0x02
 #define CURSOR_OFF 					0x00
-#define CURSOR_POS_ON 			0x01
-#define CURSOR_POS_OFF 			0x00
-#define DISPLAY_BLINKON			0x01
-#define DISPLAY_BLINKOFF		0x00
+#define CURSOR_POS_ON 			    0x01
+#define CURSOR_POS_OFF 			    0x00
+#define DISPLAY_BLINKON			    0x01
+#define DISPLAY_BLINKOFF		    0x00
 
 
 /***************************/
@@ -35,10 +35,10 @@
 //			FLAGS FOR DISPLAY ENTRY MODE
 //
 /****************************/
-#define LCD_ENTRYRIGHT 			0x00 
-#define LCD_ENTRYLEFT 			0x02
-#define LCD_SHIFTINCREMENT	0x01
-#define LCD_SHIFTDECREMENT 	0x00
+#define LCD_ENTRYRIGHT 		        0x00 
+#define LCD_ENTRYLEFT 	            0x02
+#define LCD_SHIFTINCREMENT	        0x01
+#define LCD_SHIFTDECREMENT 	        0x00
 
 /***************************/
 //
@@ -59,8 +59,8 @@
 #define LCD_4BITMODE				0x00 //			4 bit bus mode with MPU
 #define LCD_1LINEMODE				0x00 //			1-line display mode
 #define LCD_2LINEMODE				0x08 //			2-line display mode
-#define LCD_5x8DOTMODE			0x00 //			5x8 dots format display mode
-#define LCD_5x11DOTMODE			0x04 //			5x11dots format display mode
+#define LCD_5x8DOTMODE			    0x00 //			5x8 dots format display mode
+#define LCD_5x11DOTMODE			    0x04 //			5x11dots format display mode
 
 
 #endif
